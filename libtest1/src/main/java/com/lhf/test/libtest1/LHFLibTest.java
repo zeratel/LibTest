a22d848aec0beb3314afcfc445e32d7102b9593a
LHF <zeratel3000@163.com>
2015-11-22 23:00:56 +0800

    Initial commit

---
+++ libtest1/src/main/java/com/lhf/test/libtest1/LHFLibTest.java
@@ -0,0 +1,17 @@
+package com.lhf.test.libtest1;
+
+import android.util.Log;
+
+/**
+ * com.lhf.test.libtest1
+ * Created by zeratel3000
+ * on 2015 11 15/11/22 21 40
+ * description
+ */
+public class LHFLibTest {
+
+    public void printLHF(){
+        Log.i("LHF1","LHFLibTest");
+    }
+
+}
